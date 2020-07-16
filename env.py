85d3a1f9b7f7e19483d85ef5d4de0d2169448372
Yashwanth <yashuyashwanth04@gmail.com>
2020-07-16 16:53:53 +0530

    pre-processing-1

---
+++ env.py
@@ -0,0 +1,30 @@
+import pygame
+import time
+
+class Environment:
+    def __init__(self,width,height):
+        self.width=width
+        self.height=height
+        pygame.display.set_caption('Snake Game')
+        self.display=pygame.display.set_mode((width,height))
+        self.bg=pygame.transform.scale(pygame.image.load("media/background.jpg"),(width,height))
+        self.display.blit(self.bg,(0,0))
+        pygame.display.update()
+
+
+if __name__ == '__main__':
+    pygame.init()
+    env = Environment(600,400)
+    game_over = False
+    while not game_over:
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                game_over=True
+                
+    pygame.quit()
+    quit()
+
+
+
+        
+
