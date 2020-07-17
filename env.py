0b3a100ab4559c5b30b2c4edab00e193e2c3dec7
Yashwanth <yashuyashwanth04@gmail.com>
2020-07-17 17:31:36 +0530

    pre-processing-2

--- env.py
+++ env.py
@@ -1,5 +1,6 @@
 import pygame
 import time
+import random
 
 class Environment:
     def __init__(self,width,height):
@@ -11,16 +12,43 @@ class Environment:
         self.display.blit(self.bg,(0,0))
         pygame.display.update()
 
+# class Player:
+#     def __init__(self):
+#         self.x=
+#         self.body =[]
+
+class Food:
+    def __init__(self,env):
+        self.x_food=200
+        self.y_food=300
+        pygame.draw.rect(env.display,(0,255,0),[self.x_food,self.y_food,10,10])
+        pygame.display.update()
+    def new_spawn(self,env):
+        env.display.fill((255,255,255))
+        pygame.display.update()
+        self.x_food=random.randint(10,env.width-20)
+        self.y_food=random.randint(10,env.height-20)
+        env.display.blit(env.bg,(0,0))
+        pygame.display.update()
+        pygame.draw.rect(env.display,(0,255,0),[self.x_food,self.y_food,10,10])
+        pygame.display.update()
+
+         
+
+
 
 if __name__ == '__main__':
     pygame.init()
     env = Environment(600,400)
     game_over = False
+    food=Food(env)
     while not game_over:
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 game_over=True
-            print(event)
+        if random.random()<=0.5:
+            food.new_spawn(env)
+
                 
     pygame.quit()
     quit()
