f20ed1e4e9423ad576bfa88866e2e9611ac78fcb
Yashwanth <yashuyashwanth04@gmail.com>
2020-07-18 18:32:17 +0530

    pre-processing-3

--- env.py
+++ env.py
@@ -8,14 +8,30 @@ class Environment:
         self.height=height
         pygame.display.set_caption('Snake Game')
         self.display=pygame.display.set_mode((width,height))
-        self.bg=pygame.transform.scale(pygame.image.load("media/background.jpg"),(width,height))
-        self.display.blit(self.bg,(0,0))
+        self.bg=pygame.transform.scale(pygame.image.load("media/background.jpg"),(width-50,height-50))
+        self.display.fill((255,255,255))
+        self.display.blit(self.bg,(25,5))
         pygame.display.update()
+        self.player=Player()
+        self.food=Food(self)
 
-# class Player:
-#     def __init__(self):
-#         self.x=
-#         self.body =[]
+class Player:
+    def __init__(self):
+        self.x=200
+        self.y=200
+        self.body =[]
+        self.body.append([self.x,self.y])
+        self.score=1
+        self.crash=False
+
+    def display(self,env):
+        for i in range(0,self.score):
+            pygame.draw.rect(env.display,(0,0,0),[self.body[i][0],self.body[i][1],10,10])
+
+    def move(self):
+        pass
+    
+
 
 class Food:
     def __init__(self,env):
@@ -26,12 +42,15 @@ class Food:
     def new_spawn(self,env):
         env.display.fill((255,255,255))
         pygame.display.update()
-        self.x_food=random.randint(10,env.width-20)
-        self.y_food=random.randint(10,env.height-20)
-        env.display.blit(env.bg,(0,0))
-        pygame.display.update()
-        pygame.draw.rect(env.display,(0,255,0),[self.x_food,self.y_food,10,10])
-        pygame.display.update()
+        self.x_food=random.randrange(35,env.width-45,10)
+        self.y_food=random.randrange(15,env.height-65,10)
+        if [self.x_food,self.y_food] in env.player.body:
+            self.new_spawn(env)
+        else:
+            env.display.blit(env.bg,(25,5))
+            pygame.display.update()
+            pygame.draw.rect(env.display,(0,255,0),[self.x_food,self.y_food,10,10])
+            pygame.display.update()
 
          
 
@@ -40,14 +59,20 @@ class Food:
 if __name__ == '__main__':
     pygame.init()
     env = Environment(600,400)
+    player=env.player
+    food=env.food
+    player.display(env)
+    pygame.display.update()
     game_over = False
-    food=Food(env)
     while not game_over:
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 game_over=True
         if random.random()<=0.5:
             food.new_spawn(env)
+            player.display(env)
+            pygame.display.update()
+        time.sleep(1)
 
                 
     pygame.quit()
